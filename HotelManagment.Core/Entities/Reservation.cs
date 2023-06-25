4ce2bf7304d79ef79fc73e1d176b2a9268f4e4fb
Bubbler <104278936+Bubb1err@users.noreply.github.com>
2023-06-25 23:46:45 +0300

    some changes

--- HotelManagment.Core/Entities/Reservation.cs
+++ HotelManagment.Core/Entities/Reservation.cs
@@ -4,11 +4,10 @@ public class Reservation
 {
   public int Id { get; set; }
   public DateTime StartDate { get; set; }
-  public DateTime EndDate { get; set; }
-  public RoomType RoomType { get; set; }
+  public DateTime EndDate { get; set; } 
+  // Relations
   public int RoomId { get; set; }
   public Room Room { get; set; }
   public int VisitorId { get; set; }
   public Visitor Visitor { get; set; }
-
 }
